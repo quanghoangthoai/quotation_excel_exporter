43b08e81917d6a0ffd16a00ce9e63749f4388fde
quanghoangthoai <48936004+quanghoangthoai@users.noreply.github.com>
2025-04-10 14:14:53 +0700

    Initial commit for quotation_excel_exporter

---
+++ quotation_excel_exporter/hooks.py
@@ -0,0 +1,8 @@
+# hooks.py
+
+app_name = "quotation_excel_exporter"
+app_title = "Quotation Excel Exporter"
+app_publisher = "Your Name"
+app_description = "Export quotations to Excel using template"
+app_email = "you@example.com"
+app_license = "MIT"
