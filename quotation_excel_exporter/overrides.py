82793bcd9115437e46274abaf232a9a04d22d905
quanghoangthoai <48936004+quanghoangthoai@users.noreply.github.com>
2025-04-17 16:58:45 +0700

    phan tram hoa hong

--- quotation_excel_exporter/overrides.py
+++ quotation_excel_exporter/overrides.py
@@ -1,1 +1,7 @@
+from erpnext.selling.doctype.quotation.quotation import Quotation as _Quotation
+
+class Quotation(_Quotation):
+    def validate_sales_team(self):
+        # Bỏ qua kiểm tra tổng % hoa hồng
+        return
 
