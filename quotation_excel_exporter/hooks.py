2289a125b3d7d6c027821a4ad242dcfebd56e2c6
quanghoangthoai <48936004+quanghoangthoai@users.noreply.github.com>
2025-04-17 16:57:21 +0700

    Update hooks v3

--- quotation_excel_exporter/hooks.py
+++ quotation_excel_exporter/hooks.py
@@ -1,6 +1,12 @@
+from __future__ import unicode_literals
+
 app_name = "quotation_excel_exporter"
 app_title = "Quotation Excel Exporter"
 app_publisher = "Your Name"
 app_description = "Export quotations to Excel using template"
 app_email = "you@example.com"
 app_license = "MIT"
+
+override_doctype_class = {
+    "Quotation": "quotation_excel_exporter.overrides.Quotation"
+}
