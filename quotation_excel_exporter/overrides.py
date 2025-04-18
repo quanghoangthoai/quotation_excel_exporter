fa9e994798f315bf8743c58053973e2c29f20330
quanghoangthoai <48936004+quanghoangthoai@users.noreply.github.com>
2025-04-18 10:25:34 +0700

    Update overrides.py

--- quotation_excel_exporter/overrides.py
+++ quotation_excel_exporter/overrides.py
@@ -1,12 +1,4 @@
 
-# Override Quotation (nếu bạn đã làm trước)
-from erpnext.selling.doctype.quotation.quotation import Quotation as _Quotation
-
-class Quotation(_Quotation):
-    def validate_sales_team(self):
-        # Bỏ kiểm tra tổng % hoa hồng
-        return
-
 # Override Sales Order
 from erpnext.selling.doctype.sales_order.sales_order import SalesOrder as _SalesOrder
 
