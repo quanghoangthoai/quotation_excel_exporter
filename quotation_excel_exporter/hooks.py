ea192c2566d35a16cad74c2de7979ede4e765074
quanghoangthoai <48936004+quanghoangthoai@users.noreply.github.com>
2025-04-18 10:56:07 +0700

    Update hooks.py

--- quotation_excel_exporter/hooks.py
+++ quotation_excel_exporter/hooks.py
@@ -7,14 +7,13 @@ app_description = "Export quotations to Excel using template"
 app_email = "you@example.com"
 app_license = "MIT"
 
-# Nếu bạn vẫn muốn dùng override_doctype_class, để nguyên hoặc bỏ cũng được
 override_doctype_class = {
     "Sales Order": "quotation_excel_exporter.overrides.SalesOrder",
 }
 
-# Thêm hook để chạy trước khi validate
 doc_events = {
     "Sales Order": {
         "before_validate": "quotation_excel_exporter.overrides.disable_commission_validation"
     }
 }
+
