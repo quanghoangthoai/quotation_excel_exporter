41393bdb472ef7173726a004beb79a574d8d9a62
quanghoangthoai <48936004+quanghoangthoai@users.noreply.github.com>
2025-04-18 10:47:59 +0700

    Update overrides.py

--- quotation_excel_exporter/overrides.py
+++ quotation_excel_exporter/overrides.py
@@ -1,8 +1,12 @@
+# apps/quotation_excel_exporter/quotation_excel_exporter/overrides.py
 
-# Override Sales Order
 from erpnext.selling.doctype.sales_order.sales_order import SalesOrder as _SalesOrder
+
+def disable_commission_validation(doc, method):
+    # Override method validate_sales_team trên instance để no-op
+    doc.validate_sales_team = lambda: None
 
 class SalesOrder(_SalesOrder):
     def validate_sales_team(self):
-        # Bỏ kiểm tra tổng % hoa hồng
+        # Ghi đè hoàn toàn ở class level, no-op
         return
