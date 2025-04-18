7d02e806b4d77aaf74e91b81d0cded5b1fd5380a
quanghoangthoai <48936004+quanghoangthoai@users.noreply.github.com>
2025-04-18 11:14:56 +0700

    Update overrides.py

--- quotation_excel_exporter/overrides.py
+++ quotation_excel_exporter/overrides.py
@@ -1,15 +1,1 @@
-# apps/quotation_excel_exporter/quotation_excel_exporter/overrides.py
 
-from erpnext.selling.doctype.sales_order.sales_order import SalesOrder as _SalesOrder
-
-def disable_commission_validation(doc, method=None):
-    """
-    Called via doc_events before_validate.
-    Overrides the validate_sales_team method to skip commission validation.
-    """
-    doc.validate_sales_team = lambda: None
-
-class SalesOrder(_SalesOrder):
-    def validate_sales_team(self):
-        # Skip the default 100% commission rate validation
-        pass
