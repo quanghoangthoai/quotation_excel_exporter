15e7b5c3a15001fd874c60c8516dd169ac3e02b0
quanghoangthoai <48936004+quanghoangthoai@users.noreply.github.com>
2025-04-25 09:30:48 +0700

    Update sidebar_custom.js

--- quotation_excel_exporter/public/files/JS/sidebar_custom.js
+++ quotation_excel_exporter/public/files/JS/sidebar_custom.js
@@ -1,13 +1,32 @@
-$(document).ready(function() {
-    // Mở tất cả menu con mặc định
-    $('.sidebar-menu .tree-toggle').each(function() {
-        $(this).removeClass('closed').addClass('opened');
-        $(this).next('.tree-children').show();
-    });
+// Monkey-patch lại phương thức setup_sidebar
+frappe.ui.Sidebar.prototype.setup_sidebar = (function(orig) {
+  return function() {
+    // gọi bản gốc để dựng sidebar
+    orig.apply(this, arguments);
 
-    // Giữ chức năng thu gọn khi nhấp
-    $('.sidebar-menu .tree-toggle').click(function() {
-        $(this).toggleClass('opened closed');
-        $(this).next('.tree-children').slideToggle();
-    });
-});
+    const $sb = this.$sidebar;
+
+    // MỞ TẤT CẢ menu con ngay lập tức
+    $sb.find('.side-nav-item-with-child')
+       .addClass('open')
+       .find('> .indicator')
+       .text('▾');
+    $sb.find('.nested').show();
+
+    // BIND lại nút collapse sau khi đã remove cũ
+    $sb.find('.sidebar-toggle')
+       .off('click.customSidebar')           // tránh chồng event
+       .on('click.customSidebar', () => {
+         // ẩn/hiện submenu
+         $sb.find('.nested').slideToggle(200);
+         // cập nhật class open
+         $sb.find('.side-nav-item-with-child').toggleClass('open');
+
+         // và đổi icon tương ứng
+         $sb.find('.side-nav-item-with-child > .indicator').each(function() {
+           const $i = $(this);
+           $i.text($i.closest('.side-nav-item-with-child').hasClass('open') ? '▾' : '▸');
+         });
+       });
+  };
+})(frappe.ui.Sidebar.prototype.setup_sidebar);
