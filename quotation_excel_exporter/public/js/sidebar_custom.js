3d369737d0829ea02f2f10a4fc1e7bc100eb9797
quanghoangthoai <48936004+quanghoangthoai@users.noreply.github.com>
2025-04-25 09:39:37 +0700

    Update sidebar_custom.js

--- quotation_excel_exporter/public/js/sidebar_custom.js
+++ quotation_excel_exporter/public/js/sidebar_custom.js
@@ -1,32 +1,33 @@
-// Monkey-patch lại phương thức setup_sidebar
+// public/js/sidebar_custom.js
 frappe.ui.Sidebar.prototype.setup_sidebar = (function(orig) {
   return function() {
-    // gọi bản gốc để dựng sidebar
+    // Gọi bản gốc để render sidebar bình thường
     orig.apply(this, arguments);
 
     const $sb = this.$sidebar;
 
-    // MỞ TẤT CẢ menu con ngay lập tức
-    $sb.find('.side-nav-item-with-child')
+    // === MỞ CỨNG TẤT CẢ MENU CON (v15+) ===
+    $sb.find('.sidebar-menu .has-submenu')
        .addClass('open')
-       .find('> .indicator')
-       .text('▾');
-    $sb.find('.nested').show();
+       .children('.collapse').addClass('show')
+       .siblings('a').attr('aria-expanded', 'true')
+       .find('.dropdown-icon')
+         .removeClass('caret-right')
+         .addClass('caret-down');
 
-    // BIND lại nút collapse sau khi đã remove cũ
+    // === BIND LẠI NÚT COLLAPSE (sidebar-toggle) ===
     $sb.find('.sidebar-toggle')
-       .off('click.customSidebar')           // tránh chồng event
+       .off('.customSidebar')
        .on('click.customSidebar', () => {
-         // ẩn/hiện submenu
-         $sb.find('.nested').slideToggle(200);
-         // cập nhật class open
-         $sb.find('.side-nav-item-with-child').toggleClass('open');
-
-         // và đổi icon tương ứng
-         $sb.find('.side-nav-item-with-child > .indicator').each(function() {
-           const $i = $(this);
-           $i.text($i.closest('.side-nav-item-with-child').hasClass('open') ? '▾' : '▸');
-         });
+         // Toggle phần collapse của từng submenu
+         $sb.find('.sidebar-menu .has-submenu > .collapse')
+            .slideToggle(200);
+         // Toggle class open để icon & style cập nhật
+         $sb.find('.sidebar-menu .has-submenu')
+            .toggleClass('open');
+         // Đổi icon mũi tên
+         $sb.find('.dropdown-icon')
+            .toggleClass('caret-down caret-right');
        });
   };
 })(frappe.ui.Sidebar.prototype.setup_sidebar);
